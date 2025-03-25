df92d2ba2fd7e6b464a6539ac6d6958009870128
CorpMail <rodrigo.urrutia@rutiandco.com>
2025-03-25 01:15:00 -0300

    Agregando nuevos logs

--- src/utils/bot-message-sender.js
+++ src/utils/bot-message-sender.js
@@ -8,27 +8,28 @@ export async function botMessageSender({
 }){
     try {
         const { GRAPH_API_TOKEN, WAB_API_URL } = process.env;
-        let dataSending = {
+        let url = `${WAB_API_URL}/${receiver}/messages`;
+        let requestData = {
             messaging_product: "whatsapp",
             recipient_type: "individual",
             to: sender,
             type: type,
             ...content
         };
-        let axiosParams = {
-            method: "POST",
-            url: `${WAB_API_URL}/${receiver}/messages`,
+        let headers = {
             headers: {
                 Authorization: `Bearer ${GRAPH_API_TOKEN}`,
-            },
-            data: dataSending
+            }
         };
-        let logRegistry = {step: "Construir mensaje a enviar en botMessageSender", dataSending: axiosParams};
+        let logRegistry = {step: "Construir mensaje a enviar en botMessageSender", dataSending: requestData};
         await logRecorder(logRegistry);
-        let sending = await axios({
-            ...axiosParams
+        let sending = await axios.post({
+            url,
+            ...requestData,
+            ...headers
         });
-        console.log(sending);
+        logRegistry = { step: "Resultado de usar axios en botMessageSender", axios: sending };
+        await logRecorder(logRegistry);
         return true;
     } catch (error) {
         console.error("error details:", error);
