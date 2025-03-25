e58c06b89e98ba79b056a2677e5ecee705d2e609
CorpMail <rodrigo.urrutia@rutiandco.com>
2025-03-25 13:10:24 -0300

    companyIdHunter Fix: se obtiene el id de compañía a base de un número de telefono

--- src/utils/company-id-hunter.js
+++ src/utils/company-id-hunter.js
@@ -5,22 +5,20 @@ import { logRecorder } from "./log-recorder.js";
 export default async function companyIdHunter({wab}) {
     const businesses = await businessAndNumberHunter();
     let wabSearched;
-    let wabMatrix;
+    let wabMatrix = {};
     let logRegistry;
     logRegistry = {
         step: "Company Id Hunter: datos capturados en businessAndNumberHunter",
         business: businesses
     };
     await logRecorder(logRegistry);
-    businesses.forEach((({ data }) => {
-        data.forEach((({ id, platforms}) => {
+        businesses.forEach(({ id, platforms}) => {
             if (platforms.wab) {
                 platforms.wab.forEach(number => {
                     wabMatrix[number] = id;
                 });
             }
-        }));
-    }));
+        });
     wabSearched = wabMatrix[wab] || null;
     // const wabIndex = getCache("wabIndex") || {};
     logRegistry = {
