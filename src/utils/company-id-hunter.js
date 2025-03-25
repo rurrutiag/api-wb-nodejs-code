40554b31e423f7f55115e1a07b77067d33e096dd
CorpMail <rodrigo.urrutia@rutiandco.com>
2025-03-25 12:10:58 -0300

    companyIdHunter usa directamente la bbdd en lugar del cache

--- src/utils/company-id-hunter.js
+++ src/utils/company-id-hunter.js
@@ -1,13 +1,25 @@
 import { getCache } from "../cache/manager.js";
+import businessAndNumberHunter from "./business-and-number-hunter.js";
 import { logRecorder } from "./log-recorder.js";
 
 export default async function companyIdHunter({wab}) {
-    const wabIndex = getCache("wabIndex") || {};
+    const businesses = await businessAndNumberHunter();
+    let wabSearched;
+    let wabMatrix;
+    businesses.forEach((({ id, platforms}) => {
+        if (platforms.wab) {
+            platforms.wab.forEach(number => {
+                wabMatrix[number] = id;
+            });
+        }
+    }));
+    wabSearched = wabMatrix[wab] || null;
+    // const wabIndex = getCache("wabIndex") || {};
     let logRegistry = {
         step: "Company Id Hunter",
-        getInfo: wabIndex,
+        wabMatrix: wabSearched,
         infoRequest: wab
     };
     await logRecorder(logRegistry);
-    return wabIndex[wab] || null;
+    return wabSearched || null;
 }
